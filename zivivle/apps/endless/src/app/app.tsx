0d4da30228971e782dd941fe11782276c55ef653
sungkyoung <zivivle@gmail.com>
2024-03-02 22:15:50 +0900

    setting: 1차

--- zivivle/apps/endless/src/app/app.tsx
+++ zivivle/apps/endless/src/app/app.tsx
@@ -1,17 +1,17 @@
-import styled from '@emotion/styled';
+import styled from '@emotion/styled'
 
-import NxWelcome from './nx-welcome';
+import NxWelcome from './nx-welcome'
 
 const StyledApp = styled.div`
   // Your style here
-`;
+`
 
 export function App() {
   return (
     <StyledApp>
       <NxWelcome title="endless" />
     </StyledApp>
-  );
+  )
 }
 
-export default App;
+export default App
