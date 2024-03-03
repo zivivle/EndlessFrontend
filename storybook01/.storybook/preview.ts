67a79672e437efa8d5f7ce9e213fb5b8526f7126
sungkyoung <zivivle@gmail.com>
2024-03-03 20:31:10 +0900

    feat: Label 컴포넌트 storybook 적용

--- storybook01/.storybook/preview.ts
+++ storybook01/.storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from "@storybook/react";
+import "@fontsource-variable/open-sans";
+import "../src/index.css";
 
 const preview: Preview = {
   parameters: {
--- storybook01/tailwind.config.js
+++ storybook01/tailwind.config.js
@@ -1,42 +1,44 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extends: {
-      fontSize: {
-        xs: [
-          "12px",
-          {
-            lineHeight: "18px",
-            letterSpacing: "0",
-            fontWeight: "400",
-          },
-        ],
-        sm: [
-          "14px",
-          {
-            lineHeight: "21px",
-            letterSpacing: "0",
-            fontWeight: "400",
-          },
-        ],
-        base: [
-          "16px",
-          {
-            lineHeight: "24px",
-            letterSpacing: "0",
-            fontWeight: "400",
-          },
-        ],
-        xl: ["20px", "30px"],
-        "2xl": [
-          "24px",
-          {
-            lineHeight: "36px",
-            letterSpacing: "0",
-            fontWeight: "700",
-          },
-        ],
-      },
+    extends: {},
+    fontFamily: {
+      body: ["Open Sans Variable"],
+    },
+    fontSize: {
+      xs: [
+        "12px",
+        {
+          lineHeight: "18px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      sm: [
+        "14px",
+        {
+          lineHeight: "21px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      base: [
+        "16px",
+        {
+          lineHeight: "24px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      xl: ["20px", "30px"],
+      "2xl": [
+        "24px",
+        {
+          lineHeight: "36px",
+          letterSpacing: "0",
+          fontWeight: "700",
+        },
+      ],
     },
     colors: {
       primary: " #1d2745",
